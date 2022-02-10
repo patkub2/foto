816e69f7037061054f0a170aaf5854356f71ce95
patkub2 <patrykkubala1999@gmail.com>
2022-02-10 22:34:12 +0100

    Icons and navigation phone

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import Layout from "./components/themeplate/Layout";
 import NavBar from "./components/organisms/NavBar";
 import Beaner from "./components/organisms/Beaner";
 import Main from "./components/organisms/Main";
+import Navigation from "./components/organisms/Navigation";
 
 function App() {
   return (
     <>
       <Layout>
         <>
+          <Navigation />
           <NavBar></NavBar>
           <Beaner />
           <Main />
--- src/components/organisms/Navigation.tsx
+++ src/components/organisms/Navigation.tsx
@@ -1,9 +1,45 @@
 import React from "react";
+import styled from "styled-components";
+import glass from "../../images/icons/glass.svg";
+import heart from "../../images/icons/heart.svg";
+import person from "../../images/icons/person.svg";
+import shopify from "../../images/icons/shopify.svg";
+import menu from "../../images/icons/menu.svg";
+const Container = styled.div`
+  //border: 1px solid red; /* BORDER TEST*/
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  background-color: ${({ theme }) => theme.colors.light};
+  color: ${({ theme }) => theme.colors.white};
+  height: 74px;
+  border-radius: 10px 10px 0 0;
+  width: 100%;
+  justify-content: space-evenly;
+  display: flex;
+  align-items: center;
+  z-index: 2;
+  box-shadow: 0px 0px 34px -16px rgba(37, 37, 37, 1);
+`;
+
+const Icon = styled.img`
+  //border: 1px solid blue; /* BORDER TEST*/
+  width: 24px;
+  height: 24px;
+`;
 
 type Props = {};
 
 const Navigation = (props: Props) => {
-  return <div></div>;
+  return (
+    <Container>
+      <Icon src={menu}></Icon>
+      <Icon src={glass}></Icon>
+      <Icon src={heart}></Icon>
+      <Icon src={person}></Icon>
+      <Icon src={shopify}></Icon>
+    </Container>
+  );
 };
 
 export default Navigation;
